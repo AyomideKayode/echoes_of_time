ce1fafd2b4cd0e93b46bb6648f0a2e1e970f90c4
AyomideKayode <ayomidekay7@gmail.com>
2024-06-23 14:15:41 +0100

    Resolved merge conflicts with user.py and content.py.

--- models/user.py
+++ models/user.py
@@ -6,7 +6,6 @@
 from models.base_model import Base, BaseModel
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.orm import relationship
-from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(BaseModel, Base):
@@ -28,24 +27,9 @@ class User(BaseModel, Base):
     __tablename__ = 'users'
     username = Column(String(150), unique=True, nullable=False)
     email = Column(String(150), unique=True, nullable=False)
-    password_hash = Column(String(128), nullable=False)  # Hashed password
     last_login = Column(DateTime, nullable=True)
     capsules = relationship('TimeCapsule', backref='user',
                             lazy=True, cascade='all, delete-orphan')
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        if 'password' in kwargs:
-            self.password = kwargs['password']
-
-    @property
-    def password(self):
-        # Prevent password from being accessed
-        raise AttributeError('password is not a readable attribute')
-
-    @password.setter
-    def password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def verify_password(self, password):
-        return check_password_hash(self.password_hash, password)
