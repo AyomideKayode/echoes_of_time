9987fe59d770a75d6460e4b409c6ea664ed3d68a
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-06-23 18:17:34 +0100

    Add get and count methods to DB storage

---
+++ api/v1/views/users.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Users view module"""
+
+
+from api.v1.views import app_views
+from flask import jsonify, request, abort
+from models import storage
+from models.user import User
+
+
+@app_views.route('/users', methods=['GET'], strict_slashes=False)
+def get_users():
+    """Retrieves the list of all User objects"""
+    users = storage.all(User)
+    return jsonify([user.to_dict() for user in users.values()])
