50049d0b5ca42b6bd31a39ac2b8db9cdb8967cf8
AyomideKayode <ayomidekay7@gmail.com>
2024-06-29 02:01:02 +0100

    Resolve env variable issue for security.🚧

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -27,6 +27,10 @@ module.exports = {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
   plugins: [
